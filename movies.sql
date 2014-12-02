0e389044d0d9f5b557455e0c6eea69b69650fe3c
ivanhdez <ivan.hdes@gmail.com>
2014-12-02 11:40:34 -0700

    Create movies.sql

---
+++ movies.sql
@@ -0,0 +1,11 @@
+INSERT INTO Pelicula (cTitulo, cTituloOriginal, cClasificacion, cCategoria, cActores, nDuracionMins, nYear, cDirector, cImagen, cUrlTrailer, cIdioma, bActiva)
+VALUES ('Grandes Héroes', 'Big Hero 6', 'A', 'Animación, Acción, Aventura', 'Ryan Potter, Scott Adsit, Jamie Chung', 102, 2014, 'Don Hall, Chris Williams', 'thumb-02.jpg', 'http://youtu.be', 'Español' 1),
+       ('Hombre Pajaro', 'Birdman', 'B15', 'Comedia, Drama', 'Michael Keaton, Zach Galifianakis, Edward Norton', 119, 2014, 'Alejandro González Iñárritu', 'thumb-03.jpg', 'http://youtube.com', 'Inglés', 1),
+       ('Interestelar', 'Interestellar', 'B', 'Aventura, Ciencia Ficción', 'Matthew McConaughey, Anne Hathaway, Jessica Chastain', 169, 2014, 'Christopher Nolan', 'thumb-04.jpg', 'link', 'Español', 1),
+       ('Paddington', 'Paddington', 'A', 'Comedia, Familia', 'Hugh Bonneville, Sally Hawkins, Julie Walters', 95, 2014, 'Paul King', 'thumb-05.jpg', 'zelda', 'Español', 1),
+       ('Guardianes de la Galaxia' 'Guardians of the Galaxy', 'B', 'Acción, Aventura, Ciencia Ficción', 'Chris Pratt, Vin Diesel, Bradley Cooper', 121, 2014, 'thumb-06.jpg', 'zelda pl0x', 'Inglés', 1),
+       ('Maléfica', 'Maleficent', 'A', 'Acción, Aventura, Familia', 'Angelina Jolie, Elle Fanning, Sharlto Copley', 97, 2014, 'Robert Stromberg', 'thumb-07.jpg', 'link', 'Espańol', 1),
+       ('X-Men: Días del futuro pasado', 'X-Men: Days of Future Past', 'B', 'Acción, Aventura, Ciencia Ficción','Patrick Stewart, Ian McKellen, Hugh Jackman', 131, 2014, 'Bryan Singer', 'thumb-08.jpg', 'link', 'Inglés', 1),
+       ('Correr o morir', 'Mazer Runner', 'B', 'Acción', 'Misterio', 'Ciencia Ficción', "Dylan O'Brien, Kaya Scodelario, Will Poulter", 113, 2014, 'Wes Ball', 'thumb-09.jpg', 'youtube', 'Espańol', 1),
+       ('Tortugas Ninja', 'Teenage Mutant Ninja Turtles', 'A', 'Acción, Aventura, Comedia', 'Megan Fox, Will Arnett, William Fichtner', 101, 2014, 'Jonathan Liebesman', 'thumb-10.jpg', 'youtube', 'Espańol', 1),
+       ('Drácula, la leyenda jamas contada', 'Dracula Untold', 'B', 'Acción, Drama, Fantasía', 'Luke Evans, Dominic Cooper, Sarah Gadon', 92, 2014, 'Gary Shore', 'thumb-11.jpg', 'youtube', 'Inglés', 1);
